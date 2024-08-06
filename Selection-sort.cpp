61fe3a34d43b17b70b73e391a4342e21f5e06042
LakshyaGitHub98 <laktripathi9839@gmail.com>
2024-08-06 18:35:22 +0530

    Adding script of selection sorting.

---
+++ Selection-sort.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+void selection_sort(int n,vector<int>arr,char ch)
+{
+    if(ch=='I'||ch=='i')
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                    arr[i]=arr[i]^arr[j];
+                    arr[j]=arr[i]^arr[j];
+                    arr[i]=arr[i]^arr[j];
+                }
+            }
+        }
+    }
+    else if(ch=='d'||ch=='D')
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]<arr[j])
+                {
+                    arr[i]=arr[i]^arr[j];
+                    arr[j]=arr[i]^arr[j];
+                    arr[i]=arr[i]^arr[j];
+                }
+            }
+        }
+    }    
+}
+int main()
+{
+    int n,a;
+    char ch;
+    cin>>n;
+    vector<int>arr;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a;
+        arr.push_back(a);
+    }
+    cout<<"Wanna sort it in increasing order(i/I) or decreasing order(d/D) :";
+    cin>>ch;
+    selection_sort(n,arr);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
